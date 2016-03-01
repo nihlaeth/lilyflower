a0fcbd1bc51487c9c3b985fcfb06f097e3ab3e12
nihlaeth <info@nihlaeth.nl>
2016-03-01 19:06:28 +0100

    Add a bunch of blocks

--- lilyflower/containers.py
+++ lilyflower/containers.py
@@ -48,3 +48,120 @@ class Score(Container):
     """Score block."""
 
     command = "\\score"
+
+
+class Staff(Container):
+
+    """Staff block."""
+
+    command = "\\staff"
+
+
+class Header(Container):
+
+    """Header block."""
+
+    command = "\\header"
+
+
+class Layout(Container):
+
+    """Layout block."""
+
+    command = "\\layout"
+
+
+class Midi(Container):
+
+    """Midi block."""
+
+    command = "\\midi"
+
+
+class Parallel(Container):
+
+    """Parallel block."""
+
+    delimiter_pre = "<<"
+    delimiter_post = ">>"
+
+
+class Voice(Container):
+
+    """Voice block."""
+
+    command = "\\voice"
+
+
+class Paper(Container):
+
+    """Paper block."""
+
+    command = "\\paper"
+
+
+class New(Container):
+
+    """New block."""
+
+    command = "\\new"
+    min_arguments = 1
+    max_arguments = 1
+
+    # TODO: we need argument validation here!
+
+
+class Lyrics(Container):
+
+    """Lyrics block."""
+
+    command = "\\lyrics"
+
+
+class AddLyrics(Container):
+
+    """AddLyrics block."""
+
+    command = "\\addlyrics"
+
+
+class LyricsTo(Container):
+
+    """LyricsTo block."""
+
+    command = "\\lyricto"
+
+
+class Absolute(Container):
+
+    """Absolute block."""
+
+    command = "\\absolute"
+
+
+class Relative(Container):
+
+    """Relative block."""
+
+    command = "\\relative"
+    max_arguments = 1
+
+    # TODO: we need argument validation here!
+
+
+class Transpose(Container):
+
+    """Transpose block."""
+
+    command = "\\transpose"
+    min_arguments = 2
+    max_arguments = 2
+
+    # TODO: we need argument validation here!
+
+
+class Markup(Container):
+
+    """Markup block."""
+
+    command = "\\markup"

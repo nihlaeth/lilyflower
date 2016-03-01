8d5228afa4f6e078aae28ec65cc2e807c771a185
nihlaeth <info@nihlaeth.nl>
2016-03-01 20:05:13 +0100

    Add tone validation exceptions

--- lilyflower/errors.py
+++ lilyflower/errors.py
@@ -6,3 +6,24 @@ class InvalidArgument(ValueError):
     """Invalid argument."""
 
     pass
+
+
+class InvalidPitch(ValueError):
+
+    """Invalid pitch."""
+
+    pass
+
+
+class InvalidOctave(ValueError):
+
+    """Invalid Octave."""
+
+    pass
+
+
+class InvalidDuration(ValueError):
+
+    """Invalid duration."""
+
+    pass

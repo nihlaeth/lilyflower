8bd8c4661efc40d7ed097a76839a8fa8dbb35c0f
nihlaeth <info@nihlaeth.nl>
2016-03-05 00:46:03 +0100

    Document Bar

--- lilyflower/commands.py
+++ lilyflower/commands.py
@@ -5,10 +5,63 @@ from lilyflower.errors import InvalidArgument
 
 class Bar(Command):
 
-    """Bar checks and delimiters."""
+    r"""
+    Bar checks and delimiters.
+
+    Usage::
+
+        Bar(bar_string)
+
+    Parameters
+    ==========
+    bar_string: str (default="|")
+        valid lilypond bar type. Custom bar types are not supported at
+        this time.
+
+    Returns
+    =======
+    None
+
+    Raises
+    ======
+    InvalidArgument:
+        when more or less than one argument is provided, and when the
+        argument is not a valid bar type.
+
+    Notes
+    =====
+
+    See Also
+    ========
+    :class:`lilyflower.command.Command`
+
+    References
+    ==========
+    `Lilypond \\bar documentation
+    <http://lilypond.org/doc/v2.18/Documentation/notation/bars#index-_005cbar-1>`_
+
+    Examples
+    ========
+    .. testsetup::
+
+        from lilyflower.commands import Bar
+        from lilyflower.errors import InvalidArgument
+
+    .. doctest::
+
+        >>> print format(Bar())
+        \bar "|"
+        >>> print format(Bar("|."))
+        \bar "|."
+        >>> try:
+        ...     Bar("invalid bar")
+        ... except InvalidArgument as e:
+        ...     print e
+        'invalid bar' not a valid bar type.
+    """
 
     _command = "\\bar"
-    _min_arguments = 1
+    _min_arguments = 0
     _max_arguments = 1
     _inline = True
 
@@ -52,8 +105,10 @@ class Bar(Command):
             ":|.S.|:-S",
             ":|.S.|:-S"]
         # TODO: add support for custom bar definitions
-        if self._arguments[0] not in valid_bars:
+        if len(self._arguments) == 0:
+            self._validated_arguments = ["\"|\""]
+        elif self._arguments[0] not in valid_bars:
             raise InvalidArgument(
-                "%s not a valid bar type." % self._arguments[0])
+                "%r not a valid bar type." % self._arguments[0])
         else:
             self._validated_arguments = ["\"%s\"" % self._arguments[0]]

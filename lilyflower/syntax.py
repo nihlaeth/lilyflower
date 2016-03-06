a99f4905dc251919a5d03e6e29e0a43b686c98e0
nihlaeth <info@nihlaeth.nl>
2016-03-06 21:55:11 +0100

    Use raw strings for less backslashes - patch submitted by sietse

--- lilyflower/syntax.py
+++ lilyflower/syntax.py
@@ -61,7 +61,7 @@ class Spec(object, DictMixin):
         """Return named tuple with all the spec info."""
         if re.match("^[a-zA-Z_]+[a-zA-Z0-9_]*$", rule_name) is None:
             raise InvalidArgument("Invalid name %r" % rule_name)
-        if re.match("^[a-zA-Z\\\\\\-/_0-9%]*$", lily_name) is None:
+        if re.match(r"^[a-zA-Z\\\-/_0-9%]*$", lily_name) is None:
             raise InvalidArgument("Invalid lilypond name %r" % lily_name)
 
         # this list will likely change in the future
@@ -222,236 +222,236 @@ stencil = Argument('stencil', None, False)
 color = Argument('color', Color, False)
 
 # markup container
-SPEC.markup('\\markup', markup_t, None, markup_c)
+SPEC.markup(r'\markup', markup_t, None, markup_c)
 
 # markup font
-SPEC.abs_fontsize('\\abs-fontsize', markup_t, (size,), markup_c)
-SPEC.bold('\\bold', markup_t, None, markup_c)
-SPEC.box('\\box', markup_t, None, markup_c)
-SPEC.caps('\\caps', markup_t, None, markup_c)
-SPEC.dynamic_font('\\dynamic-font', markup_t, None, markup_c)
-SPEC.finger_font('\\finger', markup_t, None, markup_c)
-SPEC.font_caps('\\fontCaps', markup_t, None, markup_c)
-SPEC.font_size('\\font-size', markup_t, (size,), markup_c)
-SPEC.huge('\\huge', markup_t, None, markup_c)
-SPEC.italic('\\italic', markup_t, None, markup_c)
-SPEC.large('\\large', markup_t, None, markup_c)
-SPEC.larger('\\larger', markup_t, None, markup_c)
-SPEC.magnify('\\magnify', markup_t, (string,), markup_c)
-SPEC.medium('\\medium', markup_t, None, markup_c)
-SPEC.normal_size_sub('\\normal-size-sub', markup_t, None, markup_c)
-SPEC.normal_size_super('\\normal-size-super', markup_t, None, markup_c)
-SPEC.normal_text('\\normal-text', markup_t, None, markup_c)
-SPEC.number_font('\\number', markup_t, None, markup_c)
-SPEC.replace('\\replace', markup_t, (assoc_list,), markup_c)
-SPEC.roman_font('\\roman', markup_t, None, markup_c)
-SPEC.sans_font('\\sans', markup_t, None, markup_c)
-SPEC.simple('\\simple', markup_t, None, markup_c)
-SPEC.small('\\small', markup_t, None, markup_c)
-SPEC.small_caps('\\smallCaps', markup_t, None, markup_c)
-SPEC.smaller('\\smaller', markup_t, None, markup_c)
-SPEC.sub('\\sub', markup_t, None, markup_c)
-SPEC.super('\\super', markup_t, None, markup_c)
-SPEC.teeny('\\teeny', markup_t, None, markup_c)
-SPEC.text_font('\\text', markup_t, None, markup_c)
-SPEC.tiny('\\tiny', markup_t, None, markup_c)
-SPEC.typewriter_font('\\typewriter', markup_t, None, markup_c)
-SPEC.underline('\\underline', markup_t, None, markup_c)
-SPEC.upright('\\upright', markup_t, None, markup_c)
+SPEC.abs_fontsize(r'\abs-fontsize', markup_t, (size,), markup_c)
+SPEC.bold(r'\bold', markup_t, None, markup_c)
+SPEC.box(r'\box', markup_t, None, markup_c)
+SPEC.caps(r'\caps', markup_t, None, markup_c)
+SPEC.dynamic_font(r'\dynamic-font', markup_t, None, markup_c)
+SPEC.finger_font(r'\finger', markup_t, None, markup_c)
+SPEC.font_caps(r'\fontCaps', markup_t, None, markup_c)
+SPEC.font_size(r'\font-size', markup_t, (size,), markup_c)
+SPEC.huge(r'\huge', markup_t, None, markup_c)
+SPEC.italic(r'\italic', markup_t, None, markup_c)
+SPEC.large(r'\large', markup_t, None, markup_c)
+SPEC.larger(r'\larger', markup_t, None, markup_c)
+SPEC.magnify(r'\magnify', markup_t, (string,), markup_c)
+SPEC.medium(r'\medium', markup_t, None, markup_c)
+SPEC.normal_size_sub(r'\normal-size-sub', markup_t, None, markup_c)
+SPEC.normal_size_super(r'\normal-size-super', markup_t, None, markup_c)
+SPEC.normal_text(r'\normal-text', markup_t, None, markup_c)
+SPEC.number_font(r'\number', markup_t, None, markup_c)
+SPEC.replace(r'\replace', markup_t, (assoc_list,), markup_c)
+SPEC.roman_font(r'\roman', markup_t, None, markup_c)
+SPEC.sans_font(r'\sans', markup_t, None, markup_c)
+SPEC.simple(r'\simple', markup_t, None, markup_c)
+SPEC.small(r'\small', markup_t, None, markup_c)
+SPEC.small_caps(r'\smallCaps', markup_t, None, markup_c)
+SPEC.smaller(r'\smaller', markup_t, None, markup_c)
+SPEC.sub(r'\sub', markup_t, None, markup_c)
+SPEC.super(r'\super', markup_t, None, markup_c)
+SPEC.teeny(r'\teeny', markup_t, None, markup_c)
+SPEC.text_font(r'\text', markup_t, None, markup_c)
+SPEC.tiny(r'\tiny', markup_t, None, markup_c)
+SPEC.typewriter_font(r'\typewriter', markup_t, None, markup_c)
+SPEC.underline(r'\underline', markup_t, None, markup_c)
+SPEC.upright(r'\upright', markup_t, None, markup_c)
 
 # markup align
-SPEC.center_align('\\center-align', markup_t, None, markup_c)
-SPEC.center_column('\\center-column', markup_t, None, markup_c)
-SPEC.column('\\column', markup_t, None, markup_c)
-SPEC.combine('\\combine', markup_t, (markup, markup), None)
-SPEC.concat('\\concat', markup_t, None, markup_c)
-SPEC.dir_column('\\dir-column', markup_t, None, markup_c)
-SPEC.fill_line('\\fill-line', markup_t, None, None)
+SPEC.center_align(r'\center-align', markup_t, None, markup_c)
+SPEC.center_column(r'\center-column', markup_t, None, markup_c)
+SPEC.column(r'\column', markup_t, None, markup_c)
+SPEC.combine(r'\combine', markup_t, (markup, markup), None)
+SPEC.concat(r'\concat', markup_t, None, markup_c)
+SPEC.dir_column(r'\dir-column', markup_t, None, markup_c)
+SPEC.fill_line(r'\fill-line', markup_t, None, None)
 SPEC.fill_with_pattern(
-    '\\fill-with-pattern',
+    r'\fill-with-pattern',
     markup_t,
     (space, direction, pattern, markup, markup),
     None)
 SPEC.general_align(
-    '\\general-align',
+    r'\general-align',
     markup_t,
     (axis, direction),
     markup_c)
-SPEC.halign('\\halign', markup_t, (direction,), markup_c)
-SPEC.hcenter_in('\\hcenter-in', markup_t, (length,), markup_c)
-SPEC.hspace('\\hspace', markup_t, (space,), None)
-SPEC.justify_field('\\justify-field', markup_t, (symbol,), None)
-SPEC.justify('\\justify', markup_t, None, markup_c)
-SPEC.justify_string('\\justify-string', markup_t, (string,), None)
-SPEC.left_align('\\left-align', markup_t, None, markup_c)
-SPEC.left_column('\\left-column', markup_t, None, markup_c)
-SPEC.line('\\line', markup_t, None, markup_c)
-SPEC.lower('\\lower', markup_t, (amount,), markup_c)
-SPEC.pad_around('\\pad-around', markup_t, (amount,), markup_c)
-SPEC.pad_markup('\\pad-markup', markup_t, (amount,), markup_c)
-SPEC.pad_to_box('\\pad-to-box', markup_t, (xext, yext), markup_c)
-SPEC.pad_x('\\pad-x', markup_t, (amount,), markup_c)
+SPEC.halign(r'\halign', markup_t, (direction,), markup_c)
+SPEC.hcenter_in(r'\hcenter-in', markup_t, (length,), markup_c)
+SPEC.hspace(r'\hspace', markup_t, (space,), None)
+SPEC.justify_field(r'\justify-field', markup_t, (symbol,), None)
+SPEC.justify(r'\justify', markup_t, None, markup_c)
+SPEC.justify_string(r'\justify-string', markup_t, (string,), None)
+SPEC.left_align(r'\left-align', markup_t, None, markup_c)
+SPEC.left_column(r'\left-column', markup_t, None, markup_c)
+SPEC.line(r'\line', markup_t, None, markup_c)
+SPEC.lower(r'\lower', markup_t, (amount,), markup_c)
+SPEC.pad_around(r'\pad-around', markup_t, (amount,), markup_c)
+SPEC.pad_markup(r'\pad-markup', markup_t, (amount,), markup_c)
+SPEC.pad_to_box(r'\pad-to-box', markup_t, (xext, yext), markup_c)
+SPEC.pad_x(r'\pad-x', markup_t, (amount,), markup_c)
 SPEC.put_adjacent(
-    '\\put-adjacent',
+    r'\put-adjacent',
     markup_t,
     (axis, direction, markup, markup),
     None)
-SPEC.raise_markup('\\raise', markup_t, (amount,), markup_c)
-SPEC.right_align('\\right-align', markup_t, None, markup_c)
-SPEC.right_column('\\right-column', markup_t, None, markup_c)
-SPEC.rotate('\\rotate', markup_t, (angle,), markup_c)
-SPEC.translate('\\translate', markup_t, (offset,), markup_c)
+SPEC.raise_markup(r'\raise', markup_t, (amount,), markup_c)
+SPEC.right_align(r'\right-align', markup_t, None, markup_c)
+SPEC.right_column(r'\right-column', markup_t, None, markup_c)
+SPEC.rotate(r'\rotate', markup_t, (angle,), markup_c)
+SPEC.translate(r'\translate', markup_t, (offset,), markup_c)
 SPEC.translate_scaled(
-    '\\translate-scaled',
+    r'\translate-scaled',
     markup_t,
     (offset,),
     markup_c)
-SPEC.vcenter('\\vcenter', markup_t, None, markup_c)
-SPEC.vspace('\\vspace', markup_t, (amount,), None)
-SPEC.wordwrap_field('\\wordwrap-field', markup_t, (symbol,), None)
-SPEC.wordwrap('\\wordwrap', markup_t, None, markup_c)
-SPEC.wordwrap_string('\\wordwrap-string', markup_t, (string,), None)
+SPEC.vcenter(r'\vcenter', markup_t, None, markup_c)
+SPEC.vspace(r'\vspace', markup_t, (amount,), None)
+SPEC.wordwrap_field(r'\wordwrap-field', markup_t, (symbol,), None)
+SPEC.wordwrap(r'\wordwrap', markup_t, None, markup_c)
+SPEC.wordwrap_string(r'\wordwrap-string', markup_t, (string,), None)
 
 # markup graphic
-SPEC.arrow_head('\\arrow-head', markup_t, (direction, filled), None)
-SPEC.beam('\\beam', markup_t, (width, slope, thickness), None)
-SPEC.bracket('\\bracket', markup_t, None, markup_c)
-SPEC.circle('\\circle', markup_t, None, markup_c)
+SPEC.arrow_head(r'\arrow-head', markup_t, (direction, filled), None)
+SPEC.beam(r'\beam', markup_t, (width, slope, thickness), None)
+SPEC.bracket(r'\bracket', markup_t, None, markup_c)
+SPEC.circle(r'\circle', markup_t, None, markup_c)
 SPEC.draw_circle(
-    '\\draw-circle',
+    r'\draw-circle',
     markup_t,
     (radius, thickness, filled),
     None)
 SPEC.draw_dashed_line(
-    '\\draw-dashed-line',
+    r'\draw-dashed-line',
     markup_t,
     (destination,),
     None)
 SPEC.draw_dotted_line(
-    '\\draw-dotted-line',
+    r'\draw-dotted-line',
     markup_t,
     (destination,),
     None)
-SPEC.draw_hline('\\draw-hline', markup_t, None, None)
-SPEC.draw_line('\\draw-line', markup_t, (destination,), markup_c)
-SPEC.ellipse('\\ellipse', markup_t, None, markup_c)
-SPEC.epsfile('\\epsfile', markup_t, (axis, size, file_name), None)
-SPEC.filled_box('\\filled-box', markup_t, (xext, yext, blot), None)
-SPEC.hbracket('\\hbracket', markup_t, None, markup_c)
-SPEC.oval('\\oval', markup_t, None, markup_c)
-SPEC.parenthesize('\\parenthesize', markup_t, None, markup_c)
-SPEC.path('\\path', markup_t, (thickness, commands), None)
-SPEC.postscript('\\postscript', markup_t, (string,), None)
-SPEC.rounded_box('\\rounded-box', markup_t, None, markup_c)
-SPEC.scale('\\scale', markup_t, (factor,), markup_c)
-SPEC.triangle('\\triangle', markup_t, (filled,), None)
-SPEC.with_url('\\with-url', markup_t, (url,), markup_c)
+SPEC.draw_hline(r'\draw-hline', markup_t, None, None)
+SPEC.draw_line(r'\draw-line', markup_t, (destination,), markup_c)
+SPEC.ellipse(r'\ellipse', markup_t, None, markup_c)
+SPEC.epsfile(r'\epsfile', markup_t, (axis, size, file_name), None)
+SPEC.filled_box(r'\filled-box', markup_t, (xext, yext, blot), None)
+SPEC.hbracket(r'\hbracket', markup_t, None, markup_c)
+SPEC.oval(r'\oval', markup_t, None, markup_c)
+SPEC.parenthesize(r'\parenthesize', markup_t, None, markup_c)
+SPEC.path(r'\path', markup_t, (thickness, commands), None)
+SPEC.postscript(r'\postscript', markup_t, (string,), None)
+SPEC.rounded_box(r'\rounded-box', markup_t, None, markup_c)
+SPEC.scale(r'\scale', markup_t, (factor,), markup_c)
+SPEC.triangle(r'\triangle', markup_t, (filled,), None)
+SPEC.with_url(r'\with-url', markup_t, (url,), markup_c)
 
 # markup music
 SPEC.custom_tab_clef(
-    '\\customTabClef',
+    r'\customTabClef',
     markup_t,
     (num_strings, staff_space),
     None)
-SPEC.doubleflat('\\doubleflat', markup_t, None, None)
-SPEC.doublesharp('\\doublesharp', markup_t, None, None)
-SPEC.fermata('\\fermata', markup_t, None, None)
-SPEC.flat('\\flat', markup_t, None, None)
-SPEC.musicglyph('\\musicglyph', markup_t, (glyph_name,), None)
-SPEC.natural('\\natural', markup_t, None, None)
+SPEC.doubleflat(r'\doubleflat', markup_t, None, None)
+SPEC.doublesharp(r'\doublesharp', markup_t, None, None)
+SPEC.fermata(r'\fermata', markup_t, None, None)
+SPEC.flat(r'\flat', markup_t, None, None)
+SPEC.musicglyph(r'\musicglyph', markup_t, (glyph_name,), None)
+SPEC.natural(r'\natural', markup_t, None, None)
 SPEC.note_by_number(
-    '\\note-by-number',
+    r'\note-by-number',
     markup_t,
     (log, dot_count, direction),
     None)
-SPEC.note('\\note', markup_t, (duration, direction), None)
+SPEC.note(r'\note', markup_t, (duration, direction), None)
 SPEC.rest_by_number(
-    '\\rest-by-number',
+    r'\rest-by-number',
     markup_t,
     (log, dot_count),
     None)
-SPEC.rest('\\rest', markup_t, (duration,), None)
-SPEC.score('\\score', markup_t, None, music_c)
-SPEC.semiflat('\\semiflat', markup_t, None, None)
-SPEC.semisharp('\\semisharp', markup_t, None, None)
-SPEC.sesquiflat('\\sesquiflat', markup_t, None, None)
-SPEC.sesquisharp('\\sesquisharp', markup_t, None, None)
-SPEC.sharp('\\sharp', markup_t, None, None)
-SPEC.tied_lyric('\\tied-lyric', markup_t, (string,), None)
+SPEC.rest(r'\rest', markup_t, (duration,), None)
+SPEC.score(r'\score', markup_t, None, music_c)
+SPEC.semiflat(r'\semiflat', markup_t, None, None)
+SPEC.semisharp(r'\semisharp', markup_t, None, None)
+SPEC.sesquiflat(r'\sesquiflat', markup_t, None, None)
+SPEC.sesquisharp(r'\sesquisharp', markup_t, None, None)
+SPEC.sharp(r'\sharp', markup_t, None, None)
+SPEC.tied_lyric(r'\tied-lyric', markup_t, (string,), None)
 
 # instrument specific markup
-SPEC.fret_diagram('\\fret-diagram', markup_t, (definition_string,), None)
+SPEC.fret_diagram(r'\fret-diagram', markup_t, (definition_string,), None)
 SPEC.fret_diagram_terse(
-    '\\fret_diagram_terse',
+    r'\fret_diagram_terse',
     markup_t,
     (definition_string,),
     None)
 SPEC.fret_diagram_verbose(
-    '\\fret-diagram-verbose',
+    r'\fret-diagram-verbose',
     markup_t,
     (markings_list,),
     None)
-SPEC.harp_pedal('\\harp-pedal', markup_t, (definition_string,), None)
+SPEC.harp_pedal(r'\harp-pedal', markup_t, (definition_string,), None)
 SPEC.woodwind_diagram(
-    '\\woodwind-diagram',
+    r'\woodwind-diagram',
     markup_t,
     (instrument, user_draw_commands),
     None)
 
 # according registers markup
-SPEC.discant('\\discant', markup_t, (name,), None)
-SPEC.free_bass('\\freeBass', markup_t, (name,), None)
-SPEC.std_bass('\\stdBass', markup_t, (name,), None)
-SPEC.std_bass_iv('\\stdBassIV', markup_t, (name,), None)
-SPEC.std_bass_v('\\stdBassV', markup_t, (name,), None)
-SPEC.std_bass_vi('\\stdBassVI', markup_t, (name,), None)
+SPEC.discant(r'\discant', markup_t, (name,), None)
+SPEC.free_bass(r'\freeBass', markup_t, (name,), None)
+SPEC.std_bass(r'\stdBass', markup_t, (name,), None)
+SPEC.std_bass_iv(r'\stdBassIV', markup_t, (name,), None)
+SPEC.std_bass_v(r'\stdBassV', markup_t, (name,), None)
+SPEC.std_bass_vi(r'\stdBassVI', markup_t, (name,), None)
 
 # other markup
 SPEC.auto_footnote(
-    '\\auto-footnote',
+    r'\auto-footnote',
     markup_t,
     (markup, footnote),
     None)
-SPEC.backslashed_digit('\\backslashed-digit', markup_t, (num,), None)
-SPEC.char('\\char', markup_t, (num,), None)
-SPEC.eyeglasses('\\eyeglasses', markup_t, None, None)
-SPEC.footnote('\\footnote', markup_t, (markup, footnote), None)
-SPEC.fraction('\\fraction', markup_t, (markup, markup), None)
-SPEC.fromproperty('\\fromproperty', markup_t, (symbol,), None)
-SPEC.leftbrace('\\leftbrace', markup_t, (size,), None)
-SPEC.lookup('\\lookup', markup_t, (glyph_name,), None)
-SPEC.markalphabet('\\markalphabet', markup_t, (num,), None)
-SPEC.markletter('\\markletter', markup_t, (num,), None)
-SPEC.null('\\null', markup_t, None, None)
-SPEC.on_the_fly('\\on-the-fly', markup_t, (procedure,), markup_c)
-SPEC.override('\\override', markup_t, (new_property,), markup_c)
-SPEC.page_link('\\page-link', markup_t, (page_number,), markup_c)
+SPEC.backslashed_digit(r'\backslashed-digit', markup_t, (num,), None)
+SPEC.char(r'\char', markup_t, (num,), None)
+SPEC.eyeglasses(r'\eyeglasses', markup_t, None, None)
+SPEC.footnote(r'\footnote', markup_t, (markup, footnote), None)
+SPEC.fraction(r'\fraction', markup_t, (markup, markup), None)
+SPEC.fromproperty(r'\fromproperty', markup_t, (symbol,), None)
+SPEC.leftbrace(r'\leftbrace', markup_t, (size,), None)
+SPEC.lookup(r'\lookup', markup_t, (glyph_name,), None)
+SPEC.markalphabet(r'\markalphabet', markup_t, (num,), None)
+SPEC.markletter(r'\markletter', markup_t, (num,), None)
+SPEC.null(r'\null', markup_t, None, None)
+SPEC.on_the_fly(r'\on-the-fly', markup_t, (procedure,), markup_c)
+SPEC.override(r'\override', markup_t, (new_property,), markup_c)
+SPEC.page_link(r'\page-link', markup_t, (page_number,), markup_c)
 SPEC.page_ref(
-    '\\page-ref',
+    r'\page-ref',
     markup_t,
     (label, gauge, default),
     None)
 SPEC.pattern(
-    '\\pattern',
+    r'\pattern',
     markup_t,
     (count, axis, space, pattern),
     None)
 SPEC.property_recursive(
-    '\\property-recursive',
+    r'\property-recursive',
     markup_t,
     (symbol,),
     None)
-SPEC.right_brace('\\right-brace', markup_t, (size,), None)
-SPEC.slashed_digit('\\slashed-digit', markup_t, (num,), None)
-SPEC.stencil('\\stencil', markup_t, (stencil,), None)
-SPEC.strut('\\strut', markup_t, None, None)
-SPEC.transparent('\\transparent', markup_t, None, markup_c)
-SPEC.verbatim_file('\\verbatim-file', markup_t, (name,), None)
-SPEC.whiteout('\\whiteout', markup_t, None, markup_c)
-SPEC.with_color('\\with-color', markup_t, (color,), markup_c)
+SPEC.right_brace(r'\right-brace', markup_t, (size,), None)
+SPEC.slashed_digit(r'\slashed-digit', markup_t, (num,), None)
+SPEC.stencil(r'\stencil', markup_t, (stencil,), None)
+SPEC.strut(r'\strut', markup_t, None, None)
+SPEC.transparent(r'\transparent', markup_t, None, markup_c)
+SPEC.verbatim_file(r'\verbatim-file', markup_t, (name,), None)
+SPEC.whiteout(r'\whiteout', markup_t, None, markup_c)
+SPEC.with_color(r'\with-color', markup_t, (color,), markup_c)
 SPEC.with_dimensions(
-    '\\with-dimensions',
+    r'\with-dimensions',
     markup_t,
     (xext, yext),
     markup_c)
-SPEC.with_link('\\with-link', markup_t, (label,), markup_c)
+SPEC.with_link(r'\with-link', markup_t, (label,), markup_c)
